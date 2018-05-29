b557d52fe8000b7179889543c8981f736405e823
Dominik Schopper <dominik.schopper@bisnode.com>
2018-05-29 19:44:08 +0200

    hopefully made a clearer test file

--- src/app.spec.js
+++ src/app.spec.js
@@ -1,35 +1,38 @@
-/* of course this is something you should require ... */
-const fizzer = (num) => {
-    return (num % 3) ? num : 'Fizz';
+/* of course this is something you should write correctly and should require here ... */
+const fizzBuzzer = (num) => {
+    const values = ['Fizz', 'Buzz', 'FizzBuzz', num, num, num, num, num, num];
+    const lastIdx = values.length - 1;
+    const int = parseInt(Math.random() * lastIdx, 10) % lastIdx;
+    return values[int];
 }
 
-describe('input/output of the main fizzbuzz app', () => {
-    it('should return "Fizz"', () => {
-        const fizzyNumbers = [ 3, 13, 9, 43, 9003 ];
+describe('input/output of the "fizzBuzzer"', () => {
+    it('should return "Fizz" for [ 3, 6, 9, 300, 9003 ]', () => {
+        const fizzyNumbers = [ 3, 6, 9, 300, 9003 ];
         fizzyNumbers.forEach((num) => {
-            expect(fizzer(num)).toEqual('Fizz');
+            expect(fizzBuzzer(num)).toEqual('Fizz');
         });
     });
 
-    it('should return "Buzz"', () => {
+    it('should return "Buzz" for [5, 10, 20, 505, 10000]', () => {
         const buzzyNumbers = [5, 10, 20, 505, 10000];
         buzzyNumbers.forEach((num) => {
-            expect(fizzer(num)).toEqual('Buzz');
+            expect(fizzBuzzer(num)).toEqual('Buzz');
         });
     });
 
-    it('should return "FizzBuzz"', () => {
-        const fizzyAndBuzzyNumbers = [15, 30, 35, 45, 1500];
+    it('should return "FizzBuzz" for [15, 30, 45, 1500]', () => {
+        const fizzyAndBuzzyNumbers = [15, 30, 45, 1500];
         fizzyAndBuzzyNumbers.forEach((num) => {
-            expect(fizzer(num)).toEqual('FizzBuzz');
+            expect(fizzBuzzer(num)).toEqual('FizzBuzz');
         });
     });
 
 
-    it('should return the number itself', () => {
-        const nonChanginNumbers = [0, 1, 2, 4, 7, 8, 11, 14, 16, 17];
+    it('should return the number itself for [0, 1, 2, 7, 11, 17]', () => {
+        const nonChanginNumbers = [0, 1, 2, 7, 11, 17];
         nonChanginNumbers.forEach((num) => {
-            expect(fizzer(num)).toEqual(num);
+            expect(fizzBuzzer(num)).toEqual(num);
         });
     });
 
