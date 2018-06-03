f234b550e3857046ccba37f1b4322498355f66db
Dominik Schopper <dominik.schopper@bisnode.com>
2018-06-03 21:17:27 +0200

    changning app.js so it just calls the fizzbuzzer and shows its output

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
-
 const stopNumber = process.argv[2] || 20;
 
-console.log(`FizzBuzz program got the commandline parameter ${stopNumber}`);
+const fizzBuzzer = require('./fizzbuzzer');
 
 for (let i = 0; i <= stopNumber; i += 1) {
-    console.log(i);
+    console.log(i, ' => ', fizzBuzzer.transform(i));
 }
