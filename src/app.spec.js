93cf8ece63c3b7ad85df4d7eda11e60651c18ef7
Dominik Schopper <dominik.schopper@bisnode.com>
2018-06-03 21:13:09 +0200

    corrected some errors in the test

--- src/app.spec.js
+++ src/app.spec.js
@@ -1,30 +1,25 @@
 /* of course this is something you should require and that should work correctly  ... */
-const fizzBuzzer = (num) => {
-    const values = ['Fizz', 'Buzz', 'FizzBuzz', num, num, num, num];
-    const maxIdx = values.length - 1;
-    const rnd = parseInt(Math.random() * maxIdx, 10) % maxIdx;
-    return values[rnd];
-}
+const fizzBuzzer = require('./fizzbuzzer');
 
 describe('input/output of the "fizzBuzzer"', () => {
-    it('should return "Fizz" for [3, 13, 30, 31, 43, 3001]', () => {
-        const fizzyNumbers = [3, 13, 30, 31, 43, 3001];
+    it('should return "Fizz" for [3, 13, 31, 43, 3001]', () => {
+        const fizzyNumbers = [3, 13, 31, 43, 3001];
         fizzyNumbers.forEach((num) => {
-            expect(fizzBuzzer(num)).toEqual('Fizz');
+            expect(fizzBuzzer.transform(num)).toEqual('Fizz');
         });
     });
 
-    it('should return "Buzz" for [5, 10, 50, 51, 503, 10502]', () => {
-        const buzzyNumbers = [5, 10, 50, 51, 503, 10502];
+    it('should return "Buzz" for [5, 10, 50, 10502]', () => {
+        const buzzyNumbers = [5, 10, 50, 10502];
         buzzyNumbers.forEach((num) => {
-            expect(fizzBuzzer(num)).toEqual('Buzz');
+            expect(fizzBuzzer.transform(num)).toEqual('Buzz');
         });
     });
 
     it('should return "FizzBuzz" for [15, 30, 45, 51, 150]', () => {
         const fizzyAndBuzzyNumbers = [15, 30, 45, 51, 150];
         fizzyAndBuzzyNumbers.forEach((num) => {
-            expect(fizzBuzzer(num)).toEqual('FizzBuzz');
+            expect(fizzBuzzer.transform(num)).toEqual('FizzBuzz');
         });
     });
 
@@ -32,7 +27,7 @@ describe('input/output of the "fizzBuzzer"', () => {
     it('should return the number itself for [0, 1, 2, 4, 11, 14, 17]', () => {
         const nonChangingNumbers = [0, 1, 2, 4, 11, 14, 17];
         nonChangingNumbers.forEach((num) => {
-            expect(fizzBuzzer(num)).toEqual(num);
+            expect(fizzBuzzer.transform(num)).toEqual(num);
         });
     });
 
